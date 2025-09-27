4e9a71661376cf70b998a8cbf4db655dc3d93ca1
suara <suara@C>
2025-09-27 22:20:42 +0300

    Добавьте файлы проекта.

---
+++ Лаба 3/Лаба 3.cpp
@@ -0,0 +1,24 @@
+﻿#define _USE_MATH_DEFINES
+#include<iostream>
+#include<cmath>
+
+int main() {
+
+    double x, y, z, a1, a2, a3, a4, a5;
+    printf("enter x:");
+    scanf_s("%lf", &x);
+
+    printf("enter y:");
+    scanf_s("%lf", &y);
+
+    printf("enter z:");
+    scanf_s("%lf", &z);
+
+    a1 = 2*cos(x - (M_PI / 6));
+    a2 = 1+(pow(z,2)/(M_PI-(pow(z,2)/5)));
+    a3 = M_E + (pow(sin(y), 2));
+    a4 = (a1*a2) / a3;
+    double result = a4;
+    printf("result:%.6lf\n", result);
+    return 0;
+}
